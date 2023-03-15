93c63e9f35fe0e238d486d5ecf18cd5ff69a8c23
soonyear <soonyear@gate1.hpc>
2023-03-15 15:25:24 +0900

    Merge branch 'master' of ssh://manycore.uos.ac.kr:54329/deagwon/tdpp

--- Megatron-LM-2/sbatch-sub.sh
+++ Megatron-LM-2/sbatch-sub.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
-#SBATCH --nodes=3
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --partition=gpu2
-#SBATCH --nodelist=n069,n070,n071
-#SBATCH --gres=gpu:a10:4,gpu:a10:4,gpu:a10:4,gpu:a10:4
+#SBATCH --partition=gpu1
+#SBATCH --nodelist=n010
+#SBATCH --gres=gpu:rtx3090:4
 #SBATCH --cpus-per-task=28
-#SBATCH -o ./log2/%j.sbatch.%N.out         
-#SBATCH -e ./log2/%j.sbatch.%N.err         
+#SBATCH -o ./log2/%j.sbatch.%N.out         # STDOUT
+#SBATCH -e ./log2/%j.sbatch.%N.err         # STDERR
 
 #************************************************************
-GRES="gpu:a10:4"
-START_RANK=0
+MASTER_HOST=n069
+GRES="gpu:rtx3090:4"
+START_RANK=3
 #************************************************************
 
 cd $HOME/tdpp/Megatron-LM-2
 mkdir -p ./log2/$SLURM_JOB_ID
-NODE_LIST=`scontrol show hostnames $SLURM_JOB_NODELIST`
-MASTER_HOST=`echo $NODE_LIST | awk '{print $1}'`
 MASTER_ADDR=`cat /etc/hosts | grep $MASTER_HOST | awk '{print $1}'`
-echo $NODE_LIST
-echo $MASTER_ADDR
+echo "MASTER_ADDR:$MASTER_ADDR"
 
 ENROOT_SCRIPT=$(cat <<EOF
 CONTAINER_PATH="/scratch/enroot/\$UID/data/megatron-latest"
@@ -33,8 +31,10 @@ NODE_LIST=\`scontrol show hostnames \$SLURM_JOB_NODELIST\`
 node_array=(\$NODE_LIST)
 length=\${#node_array[@]}
 hostnode=\`hostname -s\`
-for (( index = $START_RANK; index < length + $START_RANK ; index++ )); do
-    node=\${node_array[\$index]}
+last_rank=\$((length + $START_RANK ))
+for (( index = $START_RANK; index <  \$last_rank; index++ )); do
+    array_index=\$((\$index - $START_RANK))
+    node=\${node_array[\$array_index]}
     if [ \$node == \$hostnode ]; then
         local_rank=\$index
     fi
@@ -48,7 +48,6 @@ enroot start --root \
             bash -c "cd /root/Megatron-LM/ && sh run_inter.sh \$local_rank
 EOF
 )
-
 
 srun --partition=$SLURM_JOB_PARTITION \
       --gres=$GRES \
