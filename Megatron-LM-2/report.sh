74ed400bb8c45dac24fb926aeb769029cd94a45b
root <root@gate1.hpc>
2023-03-15 10:13:55 +0900

    update for experiment

--- Megatron-LM-2/report.sh
+++ Megatron-LM-2/report.sh
@@ -3,7 +3,7 @@ jobid=$1 # jobid
 type=$2  # out, err, gpu
 
 if [ $type == "out" ]; then
-    watch -n 1 "tail -n 8 ./log2/$jobid/*.$type"
+    watch -n 1 "tail -n 10 ./log2/$jobid/*.$type"
 elif [ $type == "err" ]; then
     watch -n 1 "tail -n 5 ./log2/$jobid/*.$type"
 elif [ $type == "gpu" ]; then
