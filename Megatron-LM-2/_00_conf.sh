bebe9520b563d3815a841df62cd5c892ff1c19af
sunyeol <avatarhwang@gmail.com>
2023-07-19 10:36:19 +0900

    T5 unbalanced model partition works now

--- Megatron-LM-2/_00_conf.sh
+++ Megatron-LM-2/_00_conf.sh
@@ -5,23 +5,23 @@ CONTAINER_PATH="/scratch/enroot/$UID/data/megatron-latest"
 CONTAINER_NAME="megatron-latest"
 
 HOMOGENEOUS_CLUSTER=true
-MODEL="gpt2" # Bert / gpt2 / T5
+MODEL="T5" # Bert / gpt2 / T5
 NPROC_PER_NODE=4
-NNODES=2
+NNODES=4
 WORLD_SIZE=$((NPROC_PER_NODE * NNODES))
 GLOBAL_BATCH_SIZE=64
 MICRO_BATCH_SIZE=1
 TENSOR_MP_SIZE=1
 DP_SIZE=4
-PIPELINE_MP_SIZE=2
-PARTITION="12-12-12-12"
+PIPELINE_MP_SIZE=4
+PARTITION="11-13-11-13"
 NSYS=false
 PROFILE=false 
 MASTER_PORT=6000
 RELOAD_CONTAINER=false
 ZERO=false
 
-PIPELINE_MODEL_PARALLEL_SPLIT_RANK=6 # Where the encoder ends within the pipeline group
+PIPELINE_MODEL_PARALLEL_SPLIT_RANK=2 # Where the encoder ends within the pipeline group
 
 # set model specific arguments
 echo "MODEL : $MODEL"
@@ -37,7 +37,7 @@ elif [ $MODEL == "T5" ]; then
         ENCODER_NUM_LAYERS=$((NUM_LAYERS / 2))
         DECODER_NUM_LAYERS=$((NUM_LAYERS / 2))
 else
-        echo error: invalid model argument MODEL only "Bert/GPT/T5" is allowed
+        echo error: invalid model argument MODEL only "Bert/gpt2/T5" is allowed
         return 1
 fi
 
